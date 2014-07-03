88122af1a4e6fa92e23f7b54cdafae6ecb5d85e1
Daniel Schulte <daschult@microsoft.com>
2014-07-03 08:38:39 -0700

    Adding PublicDiagnostics and GenerateVpnClientPackage operations to azure-sdk-tools repository

---
+++ src/ServiceManagement/Compute/Commands.ServiceManagement/IaaS/Network/GetAzureVnetGatewayDiagnostics.cs
@@ -0,0 +1,41 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Network
+{
+    using System.Management.Automation;
+    using Management.Network.Models;
+    using Utilities.Common;
+
+    [Cmdlet(VerbsCommon.Get, "AzureVNetGatewayDiagnostics"), OutputType(typeof(ManagementOperationContext))]
+    public class GetAzureVnetGatewayDiagnostics : ServiceManagementBaseCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "Virtual network name.")]
+        public string VirtualNetworkName { get; set; }
+
+        protected override void OnProcessRecord()
+        {
+            UpdateGatewayPublicDiagnostics parameters = new UpdateGatewayPublicDiagnostics()
+            {
+                Operation = UpdateGatewayPublicDiagnosticsOperation.StopDiagnostics,
+            };
+
+            ServiceManagementProfile.Initialize();
+            ExecuteClientActionNewSM(
+                null,
+                this.CommandRuntime.ToString(),
+                () => this.NetworkClient.Gateways.UpdateDiagnostics(VirtualNetworkName, parameters));
+        }
+    }
+}
---
+++ src/ServiceManagement/Compute/Commands.ServiceManagement/IaaS/Network/NewAzureVPNClientPackage.cs
@@ -0,0 +1,44 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS
+{
+    using System.Management.Automation;
+    using Management.Network.Models;
+    using Utilities.Common;
+
+    [Cmdlet(VerbsCommon.New, "AzureVPNClientPackage"), OutputType(typeof(ManagementOperationContext))]
+    public class NewAzureVPNClientPackage : ServiceManagementBaseCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "Virtual network name.")]
+        public string VNetName { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "The desired processor architecture for the vpn client package.")]
+        public GatewayProcessorArchitecture ProcessorArchitecture { get; set; }
+
+        protected override void OnProcessRecord()
+        {
+            GatewayGenerateVpnClientPackageParameters parameters = new GatewayGenerateVpnClientPackageParameters()
+            {
+                ProcessorArchitecture = this.ProcessorArchitecture,
+            };
+
+            ServiceManagementProfile.Initialize();
+            ExecuteClientActionNewSM(
+                null,
+                this.CommandRuntime.ToString(),
+                () => this.NetworkClient.Gateways.GenerateVpnClientPackage(this.VNetName, parameters));
+        }
+    }
+}
